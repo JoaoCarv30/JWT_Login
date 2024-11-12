e8d26fd689ded98a4a2367a302f84e782cd9c42e
Joao Victor <Carvalho.joao1@gmail.com>
2024-11-12 18:22:28 -0300

    create user with hashedPassword

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -2,6 +2,7 @@
 
 import type { Request, Response } from 'express';
 import {prisma} from '../services/DataBase/prisma';
+import { hash } from 'bcrypt';
 
 export class UserController {
 
@@ -11,11 +12,13 @@ export class UserController {
     async create(req: Request, res: Response)  : Promise<Response> {
         const { name, email, password } = req.body;
 
+        const hashedPassword = await hash(password, 8);
+
         const user = await prisma.User.create({
             data: {
                 name,
                 email,
-                password
+                password : hashedPassword
             }
         });
 
